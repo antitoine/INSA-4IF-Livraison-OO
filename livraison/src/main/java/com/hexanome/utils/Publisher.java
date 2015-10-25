c7fd52e2fe39f4557b8ecb711c0e1e6ed87cc3a5
hverlin <hugues.verlin@gmail.com>
2015-10-25 10:46:13 +0100

    ajouts des interfaces publiser et subscriber

---
+++ livraison/src/main/java/com/hexanome/utils/Publisher.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hexanome.utils;
+
+/**
+ * This interface describes a Publisher object, or "data" in the model-view paradigm.
+ * It can be subclassed to represent an object that the application wants to have observed.
+ * A Publisher object can have one or more subsriber, which is an object that implements interface subsriber.
+ * After an object that implement Publisher changes, it has to call to the update method of the subscribers.
+ * The order in which notifications will be delivered is unspecified.
+ */
+public interface Publisher {
+    public void addSubscriber(Subscriber s);
+    public void removeSubscriber(Subscriber s);
+    public void notifySubsrciber();
+    public void clearSubscribers();
+}
